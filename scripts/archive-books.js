71e0491d1317c8bae4fda94807da6c66e528b321
Atanas Atanasov <naso.003@gmail.com>
2019-11-20 23:42:31 +0200

    moved content in html, updated script

--- scripts/archive-books.js
+++ scripts/archive-books.js
@@ -12,19 +12,15 @@
                 </span>`
     }
 
-    let books = [
-        { "bookUrl": "https://www.goodreads.com/book/show/44420517", "picUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1552458877l/44420517._SY400_.jpg", "to": "bibi" },
-        { "bookUrl": "https://www.goodreads.com/book/show/42525694", "picUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1540811475l/42525694._SY400_.jpg", "to": "kosta" },
-        { "bookUrl": "https://www.goodreads.com/book/show/11219658", "picUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1303810386l/11219658._SY400_.jpg", "to": "gogo" },
-        { "bookUrl": "https://www.goodreads.com/book/show/34218533", "picUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1486751252l/34218533._SY400_.jpg", "to": "krasi" },
-        { "bookUrl": "https://www.goodreads.com/book/show/21075609", "picUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1401541299l/21075609._SY400_.jpg", "to": "misho" },
-        { "bookUrl": "https://www.goodreads.com/book/show/15756255", "picUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1342531486l/15756255._SY400_.jpg", "to": "krasi" }
-    ];
-
+    let elements = document.getElementsByTagName("book");
     let books_markup = '';
-    books.forEach((book) => {
-        books_markup += getBookMarkup(book.bookUrl, book.picUrl, book.to);
-    });
+
+    for (let i = 0; i < elements.length; i++) {
+        let bookUrl = elements[i].getAttribute("bookUrl");
+        let picUrl = elements[i].getAttribute("picUrl");
+        let to = elements[i].getAttribute("to");
+        books_markup += getBookMarkup(bookUrl, picUrl, to);
+    }
 
     document.querySelector('.js-books-holder').innerHTML = books_markup;
     
--- scripts/books.js
+++ scripts/books.js
@@ -11,17 +11,14 @@
                 </span>`
     }
 
-    let books = [
-        { "bookUrl": "https://www.goodreads.com/book/show/44428132", "picUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1552556883l/44428132._SY400_.jpg" },
-        { "bookUrl": "https://www.goodreads.com/book/show/39345484", "picUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1521719318l/39345484._SY400_.jpg" },
-        { "bookUrl": "https://www.goodreads.com/book/show/23346490", "picUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1435515337l/23346490._SY400_.jpg" },
-        { "bookUrl": "https://www.goodreads.com/book/show/17553446", "picUrl": "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1376381888l/17553446._SY400_.jpg" }
-    ];
-
+    let elements = document.getElementsByTagName("book");
     let books_markup = '';
-    books.forEach((book) => {
-        books_markup += getBookMarkup(book.bookUrl, book.picUrl);
-    });
+
+    for (let i = 0; i < elements.length; i++) {
+        let bookUrl = elements[i].getAttribute("bookUrl");
+        let picUrl = elements[i].getAttribute("picUrl");
+        books_markup += getBookMarkup(bookUrl, picUrl);
+    }
 
     document.querySelector('.js-books-holder').innerHTML = books_markup;
     
